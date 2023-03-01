d2b9216b7720519b3ca2902e5a3d5a8b1649be6b
Jhon Cadena <jhoncadena.23@hotmail.com>
2023-03-01 13:53:59 -0500

    segunda version parcial

--- main.js
+++ main.js
@@ -3,27 +3,47 @@ function mostrar(){
     document.getElementById("resultado").innerText=total
     
 }
-    
+
+ 
+
+
+let guardado=""   
+ 
+
+// console.log(guardado)
+function guardar(){
+    if (guardado==""){
+        guardado=document.getElementById("num_1").value
+        
+        console.log(guardado)
+    }
+
+}
+
+
 
 function operador (operacion){
     switch(operacion){
         case '+':
-            total=parseInt(num_1.value)+ parseInt(num_2.value)
+            document.getElementById("num_1").value=""
+            total=parseInt(guardado)+1;
             document.getElementById("simbolo").innerText='+';
+            
             break
         case '-':
-            total=parseInt(num_1.value)- parseInt(num_2.value)
+            total=parseInt(num_1.value)- parseInt(num_1.value)
             document.getElementById("simbolo").innerText='-'
             break
         case '*':
-            total=parseInt(num_1.value)* parseInt(num_2.value)
+            total=parseInt(num_1.value)* parseInt(num_1.value)
             document.getElementById("simbolo").innerText='*'
         break
         case '/':
-            total=parseInt(num_1.value)/ parseInt(num_2.value)
+            total=parseInt(num_1.value)/ parseInt(num_1.value)
             document.getElementById("simbolo").innerText='/'
         break
         case 'AC':
             document.getElementById("resultado").innerText=""
+            document.getElementById("num_1").value=""
     }
 }
